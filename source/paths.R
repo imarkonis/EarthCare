f33536268af9b190c259f4a5b64cee7bae881e43
strnda <strnadf@fzp.czu.cz>
2018-05-28 12:38:01 +0200

    paths v.2 - it should allow for easier use of more data and easier set up for other ppl participating in the future

--- source/paths.R
+++ source/paths.R
@@ -4,3 +4,27 @@ data_gpm_path <- "C:/Users/markonis/Documents/Yannis/Various_Proj/Grants/2018ESA
 data_eobs_path <- "C:/Users/markonis/Documents/Yannis/Various_Proj/Grants/2018ESA_Validation/rawdata/eobs/"
 data_knmi_rdr_path <- "C:/Users/markonis/Documents/Yannis/Various_Proj/Grants/2018ESA_Validation/rawdata/knmi_radar/"
 data_knmi_station_path <- "C:/Users/markonis/Documents/Yannis/Various_Proj/Grants/2018ESA_Validation/rawdata/knmi_stations/"
+
+## paths v. 2
+## from now on a new user just need to enter his/hers root dir with the data - data_path
+
+if(.Platform$OS.type == 'unix') {
+  
+  if(Sys.getenv('USER') == 'phill') {
+    
+    data_path <- tools::file_path_as_absolute('~/Desktop/test') # still just a test directory
+  }
+} else {
+  
+  if(Sys.getenv('USERNAME') == 'markonis') {
+    
+    data_path <- tools::file_path_as_absolute('C:/Users/markonis/Documents/Yannis/Various_Proj/Grants/2018ESA_Validation/rawdata')
+  }
+}
+
+dirs <- list.dirs(path = data_path, recursive = F)
+
+lapply(seq_along(dirs), function(i) assign(paste('data', gsub(paste0(data_path, '/'), '', dirs[i]), 'path', sep = '_'),
+                                           dirs[i], 
+                                           envir = .GlobalEnv))
+
