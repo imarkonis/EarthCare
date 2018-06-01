b612bf25016bf21e648cf76035426c847a3a3d0e
Yannis Markonis <imarkonis@gmail.com>
2018-06-01 15:52:09 +0200

    moving wet day examination out of comparison.R

--- bin/experiment_03_netherlands/comparison.R
+++ bin/experiment_03_netherlands/comparison.R
@@ -35,50 +35,14 @@ sum(is.na(match_wet_days)) / length(knmi_wet_days) # percentage of wet days miss
 match_wet_days_rev <- match(gpm_wet_days, knmi_wet_days)
 sum(is.na(match_wet_days_rev)) / length(gpm_wet_days) # percentage of false precipitation by satellite
 
-## Explore missed wet days in gpm data
-missed_wet_days <- knmi_wet_days[which(is.na(match_wet_days))]
-missed_wet_days_prcp <- knmi_prcp[time %in% missed_wet_days]
-
-ggplot(missed_wet_days_prcp, aes(x = prcp, col = time, group = time)) + ## Better representation here
-  geom_density() +
-  theme_bw()
-barplot(table(month(unique(missed_wet_days_prcp$time))))
-
-missed_wet_days_prcp[, sum(prcp, na.rm = T)] / knmi_prcp[, sum(prcp, na.rm = T)] #percentage of prcp sum that is missed
-missed_wet_days_prcp[, sum(prcp, na.rm = T)] 
-
-aa <- missed_wet_days_prcp[, median(prcp, na.rm = T), time]
-missed_wet_days_signif <- aa[V1 > 2, time]
-
-ggplot(missed_wet_days_prcp[time %in% missed_wet_days_signif], aes(x = prcp, col = time, group = time)) + ## Better representation here
-  geom_density() +
-  theme_bw()
-
-## Explore false wet days in gpm data
-false_wet_days <- gpm_wet_days[which(is.na(match_wet_days_rev))]
-false_wet_days_prcp <- gpm_knmi_prcp[time %in% false_wet_days]
-
-ggplot(false_wet_days_prcp, aes(x = prcp, col = time, group = time)) + ## Better representation here
-  geom_density() +
-  theme_bw()
-barplot(table(month(unique(false_wet_days_prcp$time))))
-
-false_wet_days_prcp[, sum(prcp, na.rm = T)] / gpm_knmi_prcp[, sum(prcp, na.rm = T)] #percentage of prcp sum that is false
-false_wet_days_prcp[, sum(prcp, na.rm = T)]
-
-aa <- false_wet_days_prcp[, median(prcp, na.rm = T), time]
-false_wet_days_signif <- aa[V1 > 2, time]
-
-ggplot(false_wet_days_prcp[time %in% false_wet_days_signif], aes(x = prcp, col = time, group = time)) + ## Better representation here
-  geom_density() +
-  theme_bw()
-
 ## Allign wet days & add lon/lat
 gpm_knmi_prcp <- gpm_knmi_prcp[time %in% knmi_wet_days]
 rdr_prcp <- rdr_prcp[time %in% knmi_wet_days]
 
 gpm_knmi_prcp <- merge(gpm_d_cells, gpm_knmi_prcp)
 gpm_rdr_prcp <- merge(gpm_d_cells, gpm_rdr_prcp)
+
+save(knmi_wet_days, gpm_wet_days, gpm_knmi_prcp, match_wet_days, match_wet_days_rev, file = "./data/experiment_3_wdays.rdata")
 
 ## Aggregation example for each day of 2016-09
 my_date <- as.Date("2016-09-15")
